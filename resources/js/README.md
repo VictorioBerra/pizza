45b4b7462e233fca74955ae512f37f2b910e1a25
Victorio Berra <toryberra@gmail.com>
2017-08-04 13:24:49 -0500

    Added code linting. linted code. added readme.

---
+++ resources/js/README.md
@@ -0,0 +1,6 @@
+## Pizza
+
+A simple website for all your pepperoni needs.
+
+### Contributing
+Run `npm run lint` to lint the code. (`npm install` may be needed first)
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,27 +1,26 @@
+/* global jQuery:true */
+'use-strict'
 (function ($) {
+  $(function () {
+    $('.carousel').carousel({
+      interval: false
+    })
 
-    $(function () {
-        $('.carousel').carousel({
-            interval: false
-        });
+    hideArrowsMaybe()
 
-        hideArrowsMaybe();
+    $('#carousel').on('slid.bs.carousel', '', hideArrowsMaybe)
+  })
 
-        $('#carousel').on('slid.bs.carousel', '', hideArrowsMaybe);
-
-    });
-
-    function hideArrowsMaybe() {
-        var $this = $('#carousel');
-        if ($('.carousel-inner .item:first').hasClass('active')) {
-            $this.children('.left.carousel-control').hide();
-            $this.children('.right.carousel-control').show();
-        } else if ($('.carousel-inner .item:last').hasClass('active')) {
-            $this.children('.left.carousel-control').show();
-            $this.children('.right.carousel-control').hide();
-        } else {
-            $this.children('.carousel-control').show();
-        }
+  function hideArrowsMaybe () {
+    var $this = $('#carousel')
+    if ($('.carousel-inner .item:first').hasClass('active')) {
+      $this.children('.left.carousel-control').hide()
+      $this.children('.right.carousel-control').show()
+    } else if ($('.carousel-inner .item:last').hasClass('active')) {
+      $this.children('.left.carousel-control').show()
+      $this.children('.right.carousel-control').hide()
+    } else {
+      $this.children('.carousel-control').show()
     }
-
-})(jQuery);
+  }
+})(jQuery)
